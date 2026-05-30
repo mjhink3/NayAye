4d5583be8d83e632301dd5440c601f285308585e
Michael J. Hinkle III <mjhink3@gmail.com>
2026-05-30 11:31:12 -0700

    refactor: replace assets with cookie check page
    Removed temporary testing scripts and replaced image assets with a standardized cookie check HTML page to handle restricted access or site checks.

--- test-public.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-console.log('Exists public:', fs.existsSync('public'));
-if (fs.existsSync('public')) {
-  console.log('Public contents:', fs.readdirSync('public'));
-} else {
-  // Let's create the public folder!
-  console.log('Creating public folder');
-  fs.mkdirSync('public');
-}
